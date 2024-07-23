a6d5c3e338564af845854c1797ed894a34f245be
tobomabas <tobomabas@gmail.com>
2024-07-23 18:32:33 +0200

    sprint4_HomeWork

---
+++ sprint4_homework/script.js
@@ -0,0 +1,46 @@
+addNewTaskBtn.addEventListener("click", function () {
+  //definiowanie zmiennych - pobranie elementów oraz ich stworzenie
+  const taskList = document.getElementById("taskList");
+  const inputForm = document.getElementById("newTaskInputForm");
+  const li = document.createElement("li");
+  const liValue = document.createElement("span");
+  const editField = document.createElement("input");
+  liValue.textContent = inputForm.value;
+  if (inputForm.value !== "") {
+    inputForm.value = null;
+    //funkcja do tworzenia przycisków - przyjmuje nazwe oraz fukcje
+    function createBtn(btnName, btnFunction) {
+      const button = document.createElement("button");
+      button.addEventListener("click", btnFunction);
+      button.textContent = btnName;
+      return button;
+    }
+    const deleteBtn = createBtn("Usuń", function () {
+      taskList.removeChild(li);
+    });
+    const editBtn = createBtn("Edytuj", function () {
+      li.prepend(editField);
+      //zauwazylem ze inmput do edycji ma poprzednia wartosc: ponizej ja przechwytujemy:
+      editField.value = liValue.textContent;
+      liValue.textContent = null;
+      editBtn.style.display = "none";
+      const applyBtn = createBtn("Zatwierdź zmiany", function () {
+        if (editField.value !== "") {
+          liValue.textContent = editField.value;
+          li.removeChild(editField);
+          applyBtn.style.display = "none";
+          editBtn.style.display = "inline";
+          li.appendChild(deleteBtn);
+        } else alert("Nazwa zadania nie może być pusta.");
+      });
+      li.appendChild(applyBtn);
+      li.appendChild(deleteBtn);
+    });
+    //dodawanie elementów do li:
+    li.appendChild(liValue);
+    li.appendChild(editBtn);
+    li.appendChild(deleteBtn);
+    //tworzenie li zawierającego powyższe elementy:
+    taskList.appendChild(li);
+  } else alert("Nazwa zadania nie może być pusta.");
+});
